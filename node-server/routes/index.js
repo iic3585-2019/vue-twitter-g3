92c02aa966d532312f9fde5c36e5aa85eed0f586
Andre Simon <aasimon@uc.cl>
2019-06-10 14:10:55 -0400

    clean

--- node-server/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const twitterRouter = require("./twitter");
-
-router.use("/twitter", twitterRouter);
-
-module.exports = router;
